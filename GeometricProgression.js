38b436a2ca567c1cb9cd002add320d7c4b263adb
Lohithkumaras <89640966+Lohithkumaras@users.noreply.github.com>
2021-12-26 18:56:18 +0530

    Add files via upload

---
+++ GeometricProgression.js
@@ -0,0 +1,9 @@
+var a=prompt("enter the first term to find the geometric progression");
+var r=prompt("enter the common ratio to find the geometric progression");
+var n=prompt("enter the number of term in the series");
+
+var res1=n-1;
+
+
+var res=a*n*res1*res1;
+console.log(res);
